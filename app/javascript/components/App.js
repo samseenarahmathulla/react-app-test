b1bd8e2de1e4add6668fa1919c74f00f25366681
samseena <samseenar@gmail.com>
2020-02-28 10:03:46 +0800

    created a react-app with hello world

---
+++ app/javascript/components/App.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+class App extends React.Component {
+	render () {
+		return (
+		<div>Hello World - basic react app - no functionality</div>
+		)
+	}
+}
+export default App
